7f8087143edd994443be0e8a92737aa02edb9386
Denys Bonfim <denysbonfim1@gmail.com>
2026-05-01 19:44:18 +0100

    issues: overflow of long number by long variable

--- src/validation/check_overflow.c
+++ src/validation/check_overflow.c
@@ -6,7 +6,7 @@
 /*   By: deferrei <deferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 19:06:32 by deferrei          #+#    #+#             */
-/*   Updated: 2026/05/01 19:20:08 by deferrei         ###   ########.fr       */
+/*   Updated: 2026/05/01 19:39:02 by deferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ bool	string_overflow(char *string)
 {
 	long	number;
 
-	
+	if (ft_strlen(string) >= 11)
+	{
+		return (true);
+	}
 	number = ft_atoi_safe(string);
 	if (number > INT_MAX || number < INT_MIN)
 	{
