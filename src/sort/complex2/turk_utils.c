dd102e790a2d0d7a445cf274bb49750ace417065
pmalato <pedrocoelhomalato@gmail.com>
2026-05-06 19:50:01 +0100

    estamos quase, coisa boa

--- src/sort/complex2/turk_utils.c
+++ src/sort/complex2/turk_utils.c
@@ -6,10 +6,49 @@
 /*   By: pecoelho <pecoelho@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 11:12:48 by pecoelho          #+#    #+#             */
-/*   Updated: 2026/05/06 12:44:31 by pecoelho         ###   ########.fr       */
+/*   Updated: 2026/05/06 19:49:23 by pecoelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	find_cost(t_stack **stack, int index)
+{
+	t_stack	*temp;
+	int		position;
+	int		costt;
+	int		costb;
 
+	temp = *stack;
+	position = 0;
+	while (temp && temp->index < index)
+	{
+		position++;
+		temp = temp->next;
+	}
+	return (position);
+}
+
+int	precise_b_insertion(t_stack **a, t_stack **b, t_bench_mark *bench)
+{
+	int	count_op;
+	int	size;
+	int	pos;
+
+	pos = find_cost(a, (*b)->index);
+	size = stack_size(*a);
+	count_op = 0;
+	if (pos <= size - pos)
+	{
+		while (pos-- > 0)
+			count_op += ra(a, bench);
+	}
+	else
+	{
+		pos = size - pos;
+		while (pos-- > 0)
+			count_op += rra(a, bench);
+	}
+	count_op +=pa(b, a, bench);
+	return (count_op);
+}
--- src/sort/small/sort_three.c
+++ src/sort/small/sort_three.c
@@ -6,7 +6,7 @@
 /*   By: pecoelho <pecoelho@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 19:14:17 by deferrei          #+#    #+#             */
-/*   Updated: 2026/05/06 12:44:10 by pecoelho         ###   ########.fr       */
+/*   Updated: 2026/05/06 19:05:14 by pecoelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,30 @@ int	sort_three(t_stack **a, t_bench_mark *bench)
 		op_count += rra(a, bench);
 	if ((*a)->index > (*a)->next->index)
 		op_count += sa(a, bench);
+	return (op_count);
+}
+
+int	sort_four(t_stack **a, t_stack **b, t_bench_mark *bench)
+{
+	int		op_count;
+
+	assign_index(a);
+	op_count = pb(a, b, bench);
+	op_count += sort_three(a, bench);
+	if ((*b)->index == 2)
+	{
+		op_count += rra(a, bench);
+		op_count += pa(b, a, bench);
+		op_count += ra(a, bench);
+		op_count += ra(a, bench);
+	}
+	else
+	{
+		op_count += pa(b, a, bench);
+		if ((*a)->index == 1)
+			op_count += sa(a, bench);
+		else if((*a)->index == 3)
+			op_count += 4;
+	}
 	return (op_count);
 }
