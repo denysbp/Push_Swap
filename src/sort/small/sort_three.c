3c79610ebeabeb977fcaed51457f24acb333ab18
Denys Bonfim <denysbonfim1@gmail.com>
2026-05-06 23:45:19 +0100

    merge

--- src/sort/small/sort_three.c
+++ src/sort/small/sort_three.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_three.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pecoelho <pecoelho@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: deferrei <deferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 19:14:17 by deferrei          #+#    #+#             */
-/*   Updated: 2026/05/06 12:44:10 by pecoelho         ###   ########.fr       */
+/*   Updated: 2026/05/06 23:06:35 by deferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	sort_three(t_stack **a, t_bench_mark *bench)
 	int		op_count;
 
 	if (!a || !bench)
-		return (NULL);
+		return (0);
+	op_count = 0;
 	highest = find_max(*a);
 	if (*a == highest)
 		op_count += ra(a, bench);
