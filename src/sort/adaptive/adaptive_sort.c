0049483d688fc08fa2bf3f86c4437a4612627efa
pmalato <pedrocoelhomalato@gmail.com>
2026-05-07 19:59:48 +0100

    esta quaseeeee

--- src/sort/adaptive/adaptive_sort.c
+++ src/sort/adaptive/adaptive_sort.c
@@ -6,7 +6,7 @@
 /*   By: pecoelho <pecoelho@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 13:17:05 by deferrei          #+#    #+#             */
-/*   Updated: 2026/05/07 17:01:21 by pecoelho         ###   ########.fr       */
+/*   Updated: 2026/05/07 19:58:27 by pecoelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 void	sort_choose(t_stack **stack, t_bench_mark *bench)
 {
 	float	disorder;
+	size_t	size;
 
+	size = stack_size(stack);
 	disorder = disorder_rate(*stack);
+	if (size > 1 && size <= 5)
+	{
+		tiny_sort(stack, bench);
+	}
 	if (disorder < 0.2)
 	{
 		selection_min(stack, bench);
