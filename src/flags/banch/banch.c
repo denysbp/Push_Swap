383f4b1f0708ab6de5dc38b3274dd355b371f66a
Denys Bonfim <denysbonfim1@gmail.com>
2026-05-10 13:28:49 +0100

    bench update

--- src/flags/banch/banch.c
+++ src/flags/banch/banch.c
@@ -6,7 +6,7 @@
 /*   By: deferrei <deferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 12:27:19 by deferrei          #+#    #+#             */
-/*   Updated: 2026/05/08 02:48:08 by deferrei         ###   ########.fr       */
+/*   Updated: 2026/05/10 13:25:40 by deferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,36 @@ void	print_bench(t_bench_mark *bench)
 		+ bench->rrr;
 	write(2, "[bench] disorder:  ", 19);
 	print_float(bench->disorder);
-	write(2, "\n[bench] strategy:  ", 19);
+	write(2, "\n[bench] strategy:  ", 20);
 	strategy(bench);
 	write(2, "\n[bench] total_ops:  ", 21);
 	ft_putnbr_fd(bench->total, 2);
 	write(2, "\n[bench] sa:  ", 14);
 	ft_putnbr_fd(bench->sa, 2);
-	write(2, " sb:  ", 7);
+	write(2, " sb:  ", 6);
 	ft_putnbr_fd(bench->sb, 2);
-	write(2, " ss:  ", 7);
+	write(2, " ss:  ", 6);
 	ft_putnbr_fd(bench->ss, 2);
-	write(2, " pa:  ", 7);
+	write(2, " pa:  ", 6);
 	print_bench_2(bench);
 }
 
 void	print_bench_2(t_bench_mark *bench)
 {
 	ft_putnbr_fd(bench->pa, 2);
-	write(2, " pb:  ", 7);
+	write(2, " pb:  ", 6);
 	ft_putnbr_fd(bench->pb, 2);
 	write(2, "\n[bench] ra:  ", 14);
 	ft_putnbr_fd(bench->ra, 2);
-	write(2, " rb:  ", 7);
+	write(2, " rb:  ", 6);
 	ft_putnbr_fd(bench->rb, 2);
-	write(2, " rr:  ", 7);
+	write(2, " rr:  ", 6);
 	ft_putnbr_fd(bench->rr, 2);
-	write(2, " rra:  ", 8);
+	write(2, " rra:  ", 7);
 	ft_putnbr_fd(bench->rra, 2);
-	write(2, " rrb:  ", 8);
+	write(2, " rrb:  ", 7);
 	ft_putnbr_fd(bench->rrb, 2);
-	write(2, " rrr:  ", 8);
+	write(2, " rrr:  ", 7);
 	ft_putnbr_fd(bench->rrr, 2);
 	write(2, "\n", 1);
 }
