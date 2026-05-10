260b392364132d8bcc4a5e2f254bada4eb10b0b8
pmalato <pedrocoelhomalato@gmail.com>
2026-05-10 11:47:56 +0100

    finish

--- src/sort/sort_utils.c
+++ src/sort/sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: pecoelho <pecoelho@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 19:06:12 by deferrei          #+#    #+#             */
-/*   Updated: 2026/05/08 10:03:16 by pecoelho         ###   ########.fr       */
+/*   Updated: 2026/05/10 11:47:13 by pecoelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,3 @@ size_t	get_position(t_stack *stack, t_stack *target)
 	}
 	return (0);
 }
-
